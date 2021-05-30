464c2d496ef0b2b8e3ab89802093d52bafd98969
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-05-30 06:00:16 -0700

    cards set up

--- cards/deck.go
+++ cards/deck.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
-// cCreate a new type of 'deck' which is a slice of strings
+// Create a new type of 'deck' which is a slice of strings
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"♥️", "♠️", "♦️" ,"♣️"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
 
 func (d deck) print() {
 	for i, card := range d {
