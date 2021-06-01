4b39d68e4787ec069ac2999f1d422053dc85493b
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-06-01 09:57:54 -0700

    byte slices and writeFile

--- cards/deck.go
+++ cards/deck.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of 'deck' which is a slice of strings
 type deck []string
 
+// Create a new deck of cards
 func newDeck() deck {
 	cards := deck{}
 
@@ -20,8 +25,26 @@ func newDeck() deck {
 	return cards
 }
 
+// Print cards from a deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// Deal two hands of variable hand size from a deck
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// Take a deck type and turn it into a string
+func (d deck) toString() string {
+	// convert d to a slice of strings with []string(d)
+	// turn slice of strings into a single string
+	return strings.Join([]string(d), ",")
+}
+
+// Save to a deck to a file (permission 0666 means anyone can read and write the file)
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
   cards := newDeck()
 
-  cards.print()
+  // testing dealing a hand
+  hand, remainingDeck := deal(cards, 5)
+  hand.print()
+  remainingDeck.print()
+
+  // testing converting a deck to a string
+  fmt.Println(cards.toString())
+
+  // saving deck to file
+  cards.saveToFile("my_cards")
 }
